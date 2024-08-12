68119df0373a7b0dfe3ff78197012bc01a3cb0bb
Skycheeck <Sergeyxow@yandex.ru>
2024-08-12 21:05:14 +0300

    EnemiesLeftAuthoring range

--- Assets/Scripts/ECS/Authoring/EnemiesLeftAuthoring.cs
+++ Assets/Scripts/ECS/Authoring/EnemiesLeftAuthoring.cs
@@ -2,18 +2,18 @@ using ECS.Components;
 using Unity.Entities;
 using UnityEngine;
 
-namespace  ECS.Authoring
+namespace ECS.Authoring
 {
     public class EnemiesLeftAuthoring : MonoBehaviour
     {
-        public int EnemiesLeft;
+        public int Min, Max;
 
         public class EnemiesLeftBaker : Baker<EnemiesLeftAuthoring>
         {
             public override void Bake(EnemiesLeftAuthoring authoring)
             {
                 var entity = GetEntity(TransformUsageFlags.Dynamic);
-                AddComponent(entity, new EnemiesLeft {Value = authoring.EnemiesLeft});
+                AddComponent(entity, new EnemiesLeft {Value = Random.Range(authoring.Min, authoring.Max)});
             }
         }
     }
